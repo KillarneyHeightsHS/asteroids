8f078a453a9b123e27772063ffc5a6aaa2ae3320
A B <aaron.beverley1@det.nsw.edu.au>
2025-04-30 16:35:02 +1000

    strip functionality out to get back to base code

--- space_rocks.py
+++ space_rocks.py
@@ -1,20 +1,13 @@
 import pygame
-from models import Asteroid, Spaceship, GameObject
-from util import get_random_position, load_sprite, print_text
+from util import load_sprite
 
 class Asteroids:
-    MIN_ASTEROID_DISTANCE = 250
 
     def __init__(self) -> None:
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font(None, 64)
-        self.message = ""
-        self.asteroids = []
-        self.bullets = []
-        self._play_again()
     
     def main_loop(self) -> None:
         while True:
@@ -27,83 +20,19 @@ class Asteroids:
         pygame.display.set_caption("Asteroids")
 
     def _play_again(self) -> None:
-        self.message = ""
-        self.asteroids.clear()
-        self.bullets.clear()
-        self.spaceship = Spaceship((400, 300), self.bullets.append)
-
-        asteroid_count = 0
-        while asteroid_count < 6:
-            position = get_random_position(self.screen)
-            if (position.distance_to(self.spaceship.position) > self.MIN_ASTEROID_DISTANCE):
-                self.asteroids.append(Asteroid(position, self.asteroids.append))
-                asteroid_count += 1
-
+        pass
 
     def _handle_input(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
-            elif (self.spaceship and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                self.spaceship.shoot()
-            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_r):
-                self._play_again()
-
-        is_key_pressed = pygame.key.get_pressed()
-
-        if self.spaceship:
-            if is_key_pressed[pygame.K_RIGHT]:
-                self.spaceship.rotate(clockwise=True)
-            elif is_key_pressed[pygame.K_LEFT]:
-                self.spaceship.rotate(clockwise=False)
-            elif is_key_pressed[pygame.K_UP]:
-                self.spaceship.accelerate()
-            elif is_key_pressed[pygame.K_DOWN]:
-                self.spaceship.decelerate()
-
 
     def _process_game_logic(self) -> None:
-        for game_object in self._get_game_objects():
-            game_object.move(self.screen)
-
-        if self.spaceship:
-            for asteroid in self.asteroids:
-                if asteroid.collides_with(self.spaceship):
-                    self.spaceship.destroy()
-                    self.spaceship = None
-                    self.message = "You lost!"
-                    break
-
-        for bullet in self.bullets[:]:
-            for asteroid in self.asteroids[:]:
-                if asteroid.collides_with(bullet):
-                    self.asteroids.remove(asteroid)
-                    self.bullets.remove(bullet)
-                    asteroid.split()
-                    break
-
-        for bullet in self.bullets[:]:
-            if not self.screen.get_rect().collidepoint(bullet.position):
-                self.bullets.remove(bullet)
-
-        if not self.asteroids and self.spaceship:
-            self.message = "You won!"
-
-    def _get_game_objects(self) -> list[GameObject]:
-        game_objects = [*self.asteroids, *self.bullets]
-        if self.spaceship:
-            game_objects.append(self.spaceship)
-        return game_objects
+        pass
 
     def _draw(self) -> None:
         self.screen.blit(self.background, (0, 0))
-
-        for game_object in self._get_game_objects():
-            game_object.draw(self.screen)
-        
-        if self.message:
-            print_text(self.screen, self.message, self.font)
 
         pygame.display.flip()
         self.clock.tick(60)
